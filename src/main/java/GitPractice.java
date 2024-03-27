0435de9acdc62cb28de785bce6300b21f65c610e
Fati84 <150975393+Fati84@users.noreply.github.com>
2024-03-26 22:05:28 -0400

    Update GitPractice.java

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -2,5 +2,7 @@ public class GitPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        system.out.println( "Changes made in GitHub");
+                
     }
 }
